f7cae651669abb08c778bc7061fd674772ac088c
Lucas Fernandes <fernandeslucas960@gmail.com>
2023-11-18 00:07:25 -0300

    Criando arquivo de configuração sequelize, criando migrations, configuração do server.

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const {Router} = require('express');
+const {Router} = require("express");
 
 const routes = new Router();
 
 routes.get("/health", (req, res) => {
-    return res.send({message: 'connected with success in port 3000!'});
+	return res.send({message: "connected with success in port 3000!"});
 });
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
-const routes = require('./routes');
-const express = require('express');
+require("dotenv").config();
+require("./database/index");
+const routes = require("./routes");
+const express = require("express");
 const app = express();
 const {PORT} = process.env;
 
 app.use(express.json());
 app.use(routes);
+
 app.listen(process.env.PORT, () => {
-    console.log(`server is on in port ${PORT}` );
+	console.log(`server is on in port ${PORT}` );
 });
