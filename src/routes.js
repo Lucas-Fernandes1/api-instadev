09e76015dfb7267a8d86ee3bfa96619a0a792525
Lucas Fernandes <fernandeslucas960@gmail.com>
2023-11-19 11:01:30 -0300

    Adicionando nova rota post utilizando middeware.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 
+const schemaValidator = require("./apps/middlewares/schemaValidator");
+const userSchema = require("./schema/create.user.schema.json");
 const {Router} = require("express");
+const UserController = require("./apps/controllers/UserController");
 
 const routes = new Router();
+
+routes.post("/user", schemaValidator(userSchema), UserController.createUser);
 
 routes.get("/health", (req, res) => {
 	return res.send({message: "connected with success in port 3000!"});
