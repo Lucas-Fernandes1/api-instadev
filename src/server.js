fa87b67c269ac931f14dfa8a8c691b0ac2211cc7
Lucas Fernandes <fernandeslucas960@gmail.com>
2024-01-22 21:25:01 -0300

    Inserção do cors ao projeto.

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 require("./database/index");
 const routes = require("./routes");
 const express = require("express");
+const cors = require("cors")
+
 const app = express();
+
 const {PORT} = process.env;
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 
 app.listen(process.env.PORT, () => {
