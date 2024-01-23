0b787b7981b9379639c5af035f30cd863476b06d
Lucas Fernandes <fernandeslucas960@gmail.com>
2024-01-23 14:19:09 -0300

    - Inserido configurações para upload das imagens para a plataforma backblaze; - Exclusão dos arquivos da pasta upload após envio; - Inserção de variáveis de ambiente relacionados ao backblaze.

--- src/apps/controllers/FileController.js
+++ src/apps/controllers/FileController.js
@@ -1,8 +1,60 @@
+const fsp = require("fs/promises");
+const B2 = require("backblaze-b2");
+
+const {
+	APPLICATIONKEYID, 
+	APPLICATIONKEY, 
+	BUCKETID,
+	BASE_URL_BLACKBAZE
+} = process.env
+
+const b2 = new B2({
+	applicationKeyId: APPLICATIONKEYID,
+	applicationKey: APPLICATIONKEY
+});
+
+const unlinkAsync = fsp.unlink;
+
 class FileController {
 	async upload(req, res) {
-		const {filename} = req.file;
-		
-		return res.status(200).json({url: `uploads/${filename}`});
+	
+		const filename = req.file.filename;
+		const path = req.file.path;
+
+		try {
+			
+			const file = await fsp.readFile(`uploads/${filename}`, (err, data) => {
+				
+				if (err) {
+					throw(err)
+				}
+				
+				return data;
+			});
+
+			await b2.authorize();
+
+			const { data: {uploadUrl, authorizationToken} } = await b2.getUploadUrl({
+				bucketId: BUCKETID
+			});
+
+			
+			const {data} = await b2.uploadFile({
+				uploadUrl: uploadUrl,
+				uploadAuthToken: authorizationToken,
+				fileName: filename,
+				data: file
+			});
+			
+			await unlinkAsync(path);
+
+			return res.send({url: `${BASE_URL_BLACKBAZE}${data.fileName}`});
+
+		} catch (error) {
+			console.log(error.message)
+			return res.status(400).send({message: "Failed to upload!"});
+		}
+
 	}
 }
 
